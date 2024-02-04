6e8939496fe8f24279a6d4d668086067c39ea479
sahilsaini172 <158755923+sahilsaini172@users.noreply.github.com>
2024-02-04 16:48:30 +0530

    Update Main.kt

--- Temperature Converter/src/Main.kt
+++ Temperature Converter/src/Main.kt
@@ -2,17 +2,25 @@ fun main() {
     println("Choose an option: ")
     println("1. Convert to Fahrenheit")
     println("2. Convert to Degree Celsius")
+    println("3. Convert from Degree Celsius to Kelvin")
+    println("4. Convert from Fahrenheit to Kelvin")
+    println("5. Convert from Kelvin to Degree Celsius")
+    println("6. Convert from Kelvin to Fahrenheit")
     print("Option: ")
     val optionSelected = readln().toInt()
     print("Enter the temperature: ")
     val temp = readln().toDouble()
-    println(temperatureConverter(optionSelected,temp))
+    println(temperatureConverter(optionSelected, temp))
 }
 
-fun temperatureConverter(option: Int,temp: Double): Comparable<*> {
-    val convertedTemperature = when(option) {
-        1 -> (temp * (9.0/5) + 32)
-        2 -> (temp - 32) * (5.0/9)
+fun temperatureConverter(option: Int, temp: Double): Comparable<*> {
+    val convertedTemperature = when (option) {
+        1 -> (temp * (9.0 / 5) + 32)
+        2 -> (temp - 32) * (5.0 / 9)
+        3 -> temp + 273.15
+        4 -> (temp - 32) * (5.0 / 9) + 273.15
+        5 -> temp - 273.15
+        6 -> ((temp - 273.15) * (9.0 / 5) + 32)
         else -> "Wrong Input"
     }
     return convertedTemperature
